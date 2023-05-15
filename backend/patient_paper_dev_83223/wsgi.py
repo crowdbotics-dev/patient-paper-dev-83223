e2e42b4330bbb6ad40b00b69624697b2d65d9aa4
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 00:43:57 +0000

    Initial commit.

---
+++ backend/patient_paper_dev_83223/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for patient_paper_dev_83223 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patient_paper_dev_83223.settings')
+
+application = get_wsgi_application()
